271c3206a575a2fa7c139320db937a9185c374f6
Jashan <139503673+JashanDigpal786@users.noreply.github.com>
2025-05-01 16:58:50 +0530

    update

--- rd_frontend/src/components/Login.js
+++ rd_frontend/src/components/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Login() {
   return (
     <>
-      {/* <div className='container'>
+      <div className='container'>
         <div className='row py-2 '>
             <div className='col-2'>Email:</div>
             <div className='col-10'><input className='form-control' type='email' placeholder='enter email' value={email} onChange={changeEmail}></input></div>
@@ -13,7 +13,7 @@ function Login() {
             <div className='col-10'><input className='form-control' type='password' placeholder='enter password' value={password} onChange={changePassword}></input></div>
         </div>
         <button type='submit'>Login</button>
-      </div> */}
+      </div>
     </>
   )
 }
