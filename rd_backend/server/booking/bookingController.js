e5864f142a960bfc05618115fec3d4b4d69128f2
ankit19470 <ankit.sharma85590@gmail.com>
2025-05-02 16:20:55 +0530

    added

---
+++ rd_backend/server/booking/bookingController.js
@@ -0,0 +1,106 @@
+const { model } = require("mongoose")
+const Booking = require("./bookingModel")
+addbooking = (req, res) => {
+    var validationerror = []
+    if (!req.body.itemId)
+        validationerror.push("itemId is required")
+    if (!req.body.customerId)
+        validationerror.push("customerId is required")
+    if (!req.body.accountHolderName)
+        validationerror.push("accountHolderName is required")
+    if (!req.body.accountNumber)
+        validationerror.push("accountNumber is required")
+    if (!req.body.paymentMode)
+        validationerror.push("paymentMode is required")
+    if (!req.body.cvv)
+        validationerror.push("cvv is required")
+
+    if (validationerror.length > 0) {
+        res.send({
+            status: 420,
+            success: false,
+            message: "Validation error occur",
+            error: validationerror,
+
+        })
+    }
+    else {
+        // insert
+        Booking.findOne({ accountNumber: req.body.accountNumber })
+            .then(bookdata => {
+                if (!bookdata) {
+                    // let total=Category.countDocuments()
+                    let bookingObj = new Booking()
+                    bookingObj.itemId = req.body.itemId
+                    bookingObj.customerId = req.body.customerId
+                    bookingObj.accountHolderName = req.body.accountHolderName
+                    bookingObj.paymentMode = req.body.paymentMode
+                    bookingObj.accountNumber = req.body.accountNumber
+                    bookingObj.cvv = req.body.cvv
+                    bookingObj.save()
+                        .then((saveData) => {
+                            res.send({
+                                status: 200,
+                                success: true,
+                                message: "New Booking successfully !!",
+                                data: saveData
+                            })
+                        })
+                        .catch((err) => {
+                            res.send({
+                                status: 500,
+                                success: false,
+                                message: "Internal server error",
+                                error: err.message
+                            })
+                        })
+                }
+                else {
+                    // duplycacy
+                    res.send({
+                        status: 400,
+                        success: false,
+                        message: "Record is already exist",
+
+
+                    })
+                }
+            })
+
+            .catch((err) => {
+                res.send({
+                    status: 500,
+                    success: false,
+                    message: "Internal server error",
+                    error: err.message
+                })
+            })
+
+    }
+}
+getallBooking = (req, res) => {
+    Booking.find()
+    .populate("itemId")
+    .populate("customerId")
+        .then(bookdata => {
+            res.send({
+                status: 200,
+                success: true,
+                message: "Data loaded",
+                data: bookdata
+            })
+        })
+        .catch(err => {
+            res.send({
+                status: 500,
+                success: false,
+                message: "Internal server error",
+                error: err.message
+            })
+        })
+}
+
+module.exports = {
+    addbooking,
+    getallBooking
+}
--- rd_backend/server/user/userModel.js
+++ rd_backend/server/user/userModel.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
     name: { type: String, default: null },
     email: { type: String, default: null },
     password: { type: String, default: null },
+    userType: { type: String, default: 2 },
     status: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now }
 })
