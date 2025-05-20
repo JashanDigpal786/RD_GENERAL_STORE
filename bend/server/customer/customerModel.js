ba3c93f44d99d412ec860bbe3185ed1e73e123f7
Jashan <139503673+JashanDigpal786@users.noreply.github.com>
2025-05-20 15:46:25 +0530

    ADD

--- bend/server/customer/customerModel.js
+++ bend/server/customer/customerModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const customerSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.ObjectId, ref: "user", default: null },
   name: { type: String, default: null },
