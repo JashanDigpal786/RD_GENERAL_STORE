f1297bb3b0e4ff21355e3226740dddb7993e4080
Jashan <139503673+JashanDigpal786@users.noreply.github.com>
2025-05-21 17:48:14 +0530

    add

--- rd_backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/rd_backend')
-  .then(() => {
-    console.log('db is connected!')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
